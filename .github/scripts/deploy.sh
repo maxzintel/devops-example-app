2db3784ba1e348a377aaccda38691909a4603628
s1dequest <maxzintel@gmail.com>
2022-08-24 11:03:36 -0400

    Add back apply and try deploying client.

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -48,7 +48,7 @@ k8s_deploy() {
     # popd
     cd -
     # build + apply manifest
-    kustomize build "kube/overlays/${ENV}/" # | kubectl apply -f -
+    kustomize build "kube/overlays/${ENV}/" | kubectl apply -f -
 }
 
 main
