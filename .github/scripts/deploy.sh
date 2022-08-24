97ac0d1e8cef620700d94d4ff5d9a0355b4b3143
s1dequest <maxzintel@gmail.com>
2022-08-24 10:42:28 -0400

    Test image tag setting.

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -27,11 +27,12 @@ main() {
 k8s_deploy() {
     echo -e "+++ :k8s: Deploy Env."
     cd "kube/overlays/${ENV}/"
-    kustomize edit set image "${IMAGE_REPO}=:${IMAGE_TAG}"
+    # kustomize edit set image "${IMAGE_REPO}=:${IMAGE_TAG}"
     kustomize edit set nameprefix "${DEPLOY_RELEASE}-"
     kustomize edit add label -f release:${DEPLOY_RELEASE}
     kustomize edit add label -f environment:staging
     kustomize edit add label -f app:${APP}
+    kustomize edit add label -f image_tag:${IMAGE_TAG}
 
     # get app secrets, output to temp secrets-gen file we look at in kustomization.yaml
     #  > secrets-gen.env
@@ -43,7 +44,7 @@ k8s_deploy() {
     # popd
     cd -
     # build + apply manifest
-    kustomize build "kube/overlays/${ENV}/" | kubectl apply -f -
+    kustomize build "kube/overlays/${ENV}/" # | kubectl apply -f -
 }
 
 main
